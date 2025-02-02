e47233736a8f34780da8ccfeabe88e6d7f6f323c
besoeasy <besoeasy@users.noreply.github.com>
2025-02-02 12:14:38 +0530

    refactor package.json and vite.config.js to remove vite-plugin-pwa dependency and adjust plugin configuration

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,9 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
