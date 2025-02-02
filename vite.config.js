b6b60d480985f8a137274e79293df53ff926a9df
besoeasy <besoeasy@users.noreply.github.com>
2025-02-02 10:35:13 +0530

    refactor vite.config.js to configure VitePWA for automatic updates

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 
-
- export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA()],
+export default defineConfig({
+  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
