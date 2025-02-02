16052f055dd64fe49a2482a1127d18a08e40d3b7
besoeasy <besoeasy@users.noreply.github.com>
2025-02-02 10:30:56 +0530

    refactor vite.config.js to integrate VitePWA plugin for improved PWA support

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa'
+
 
  export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
