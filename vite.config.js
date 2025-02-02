7bbcc2d88e01e8af03e18dd99a6cac4947b596b3
besoeasy <besoeasy@users.noreply.github.com>
2025-02-02 07:42:24 +0530

    add vue and tailwindcss plugins to Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
+
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
