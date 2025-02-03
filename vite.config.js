f934d06d8bacb5ca614375f18382a708d39fe88d
besoeasy <besoeasy@users.noreply.github.com>
2025-02-03 17:54:44 +0530

    Remove PWA support and related configurations from index.html and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,39 +6,8 @@ import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
 
-import { VitePWA } from "vite-plugin-pwa";
-
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    tailwindcss(),
-    VitePWA({
-      registerType: "autoUpdate",
-      includeAssets: ["favicon.svg", "favicon.ico", "robots.txt", "apple-touch-icon.png"],
-      manifest: {
-        name: "My PWA App",
-        short_name: "PWA",
-        description: "My Vue PWA",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
-      },
-    }),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
