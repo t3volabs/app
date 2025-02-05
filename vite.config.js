d2272e03f5fb3741d91eb15fe3a234748e4d14e3
besoeasy <besoeasy@users.noreply.github.com>
2025-02-06 03:13:32 +0530

    stable

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,10 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueDevTools(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
