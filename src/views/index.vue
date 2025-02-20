3adbf4d07daba6e7514c86aa7af99e7a559c9082
besoeasy <besoeasy@users.noreply.github.com>
2025-02-20 12:30:45 +0530

    stable

--- src/views/index.vue
+++ src/views/index.vue
@@ -45,18 +45,17 @@
         <p class="text-xl text-gray-700 mb-8 leading-relaxed">We believe in the power of community-driven development. That's why T3VO is open source and available for anyone to contribute to or use. Join us in shaping the future of password management!</p>
       </div>
       <div class="md:w-1/3 p-8 md:p-12 flex flex-col justify-center space-y-4">
-        <a
-          href="https://github.com/t3volabs"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="inline-flex items-center justify-center px-6 py-3 rounded-full text-base font-medium text-white bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 transition-all duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-        >
+        <a href="https://github.com/t3volabs/app" target="_blank" class="inline-flex items-center justify-center px-6 py-3 rounded-full text-base font-medium text-white bg-gradient-to-r from-blue-500 to-purple-500 hover:from-blue-600 hover:to-purple-600 transition-all duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
           <GithubIcon class="h-5 w-5 mr-2" />
           View on GitHub
         </a>
-        <a href="https://github.com/t3volabs/issues" target="_blank" rel="noopener noreferrer" class="inline-flex items-center justify-center px-6 py-3 rounded-full text-base font-medium text-gray-700 bg-white border border-gray-300 hover:bg-gray-50 transition-all duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+        <a href="https://github.com/t3volabs/app/issues" target="_blank" class="inline-flex items-center justify-center px-6 py-3 rounded-full text-base font-medium text-gray-700 bg-white border border-gray-300 hover:bg-gray-50 transition-all duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
           <AlertCircleIcon class="h-5 w-5 mr-2" />
           Report an Issue
+        </a>
+        <a href="https://t.me/t3volabs" target="_blank" class="inline-flex items-center justify-center px-6 py-3 rounded-full text-base font-medium text-gray-700 bg-blue border border-gray-300 hover:bg-blue-50 transition-all duration-200 ease-in-out transform hover:scale-105 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+          <UsersRound class="h-5 w-5 mr-2" />
+          Join Telegram
         </a>
       </div>
     </div>
@@ -67,7 +66,7 @@
 import { ref, onMounted, computed } from "vue";
 import { version } from "../../package.json";
 import { db, decryptContent } from "@/db";
-import { KeyIcon, BookmarkIcon, FileTextIcon, GithubIcon, AlertCircleIcon } from "lucide-vue-next";
+import { KeyIcon, BookmarkIcon, FileTextIcon, GithubIcon, AlertCircleIcon, UsersRound} from "lucide-vue-next";
 import { Chart as ChartJS, ArcElement, Tooltip, CategoryScale, LinearScale, PointElement, LineElement, Title } from "chart.js";
 import { Pie, Line } from "vue-chartjs";
 import { format } from "timeago.js";
@@ -84,7 +83,6 @@ const stats = computed(() => [
   { title: "Passwords", count: String(savedPasswords.value) },
   { title: "Bookmarks", count: String(savedBookmarks.value) },
   { title: "Notes", count: String(savedNotes.value) },
-
 ]);
 
 const loadData = async () => {
